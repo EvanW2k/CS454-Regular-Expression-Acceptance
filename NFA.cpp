edec1fe7cd2462dfbe4048420041fb6d9e5e306b
EvanW2k <125333331+EvanW2k@users.noreply.github.com>
2023-05-03 19:46:26 -0700

    Delta = 2d vector of pairs implementation
    coded . , *, and +. + probably needs more testing

--- NFA.cpp
+++ NFA.cpp
@@ -57,7 +57,7 @@ string infixToPrefix(string RE) {
         }
         // in case of operator
         else if (isOp(RE[i])) {
-            
+
             // when stack is empty just push
             if (s.empty()) {
                 s.push(RE[i]);
@@ -106,77 +106,138 @@ string infixToPrefix(string RE) {
     return output;
 }
 
+NFA::NFA() {
+
+
+}
 
 NFA::NFA(char operand) {
 
-    // & => epsilon / null
-    // - => empty / no transition
-    
-    vector<char> states { '&', operand};
-    vector<char> empty {'-', '-'};
-    
-    //delta holds 1 vector of 2 state NFA and an empty vector
-    _delta.push_back(states);
-    _delta.push_back(empty);
+    _finalStates.push_back(1);  // for single operand constructor state 1 will be the final state
+    _startState = 0;
 
-    //1 is operand in <states>, transition to final
-    _finalStates.push_back(1);
-    _size = 2;
+    _delta.push_back(vector<pair<int, char>> { {0,'&'} });  // push vector with epsilon transition 
+    _delta.push_back(vector<pair<int, char>> { {1, '&'}, {0, operand} });  // push vector with operand transition 
+
+    _size = _delta.size();  // 2
+  
 }
 
 // 
-NFA::NFA(NFA M1, NFA M2, char op) {  
+NFA::NFA(NFA M1, char op, NFA M2) {     // M2 has a default value to make it an optional parameter
     assert(isOp(op));
-
-    _size = M1._size + M2._size;
+    
+    // UNION
     if (op == '+') {
-        
-    } 
-    else if (op == '.') {
-        //vector1.insert( vector1.end(), vector2.begin(), vector2.end() );
-        //length of nfa 1 in empty spaces before 
-        vector<vector<char>> delta = M1.getDelta();
-        for (int i = 0; i < delta.size(); i++) {
-            delta[i].resize(_size, '-');
-        }
-        delta.resize(_size, vector<char>(_size, '-'));
-        vector<int> M1F = M1.getFinalStates();
-        vector<int> M2F = M2.getFinalStates();
-        for (int i = 0; i < M1F.size(); i++) {
-            //change final states of M1 into epsilon transitions to starting states of M2
-            delta[M1F[i]][M1._size] = '&';
-        }
-        //think further
-        for (int i = 0; i < M2F.size(); i++) {
-            delta[M1._size][M2F[i] + M1._size];
-        }
-    } 
-    else if (op == '*') {
+        // start new NFA with 1 state then copy an incremented M1 to delta, and finals
 
-    } 
-}
+        _delta.push_back(vector<pair<int, char>> {{0, '&'}});
+        _size = 1;      
+        _startState = 0;
 
-void NFA::print() {
-    cout << "Printing delta function contents of NFA: " << endl;
-    for (int i = 0; i < _delta.size(); i++) {
-        for (int j = 0; j < _delta[i].size(); j++) {
-            if (_delta[i][j] != '&') {
-                cout << "delta[" << i << "][" << j << "] = " << _delta[i][j] << " ";
+        for (int i = 0; i < M1.getSize(); i++) {
+            vector<pair<int, char>> temp = M1.getDelta()[i];    // get first vector of pairs from M1
+
+            // increment each state num in vector of pairs by 1
+            for (int j = 0; j < temp.size(); j++) {
+                get<0>(temp[j])++;
             }
+
+            _delta.push_back(temp);     // push modified vector of M2 pairs 
+            _size++;    // increment size
         }
-        cout << endl;
+
+        // copy over incremented finals from M1
+        for (int i = 0; i < M1.getFinalStates().size(); i++) {
+            _finalStates.push_back(M1.getFinalStates()[i] + 1);
+        }
+
+        // add transition from new start state to old M1 start state
+        _delta[M1.getStart() + 1].push_back({ 0,'&'});
+
+        // Now concatenate M2 into new NFA with adjusted states
+        for (int i = 0; i < M2.getSize(); i++) {
+            vector<pair<int, char>> temp = M2.getDelta()[i];    // get first vector of pairs from M2
+
+            // increment each state num in vector of pairs by size of M1
+            for (int j = 0; j < temp.size(); j++) {
+                get<0>(temp[j]) += M1.getSize() + 1;    // +1 for new start state
+            }
+
+            _delta.push_back(temp);     // push modified vector of M2 pairs 
+            _size++;    // increment size
+        }
+
+        // copy over incremented finals from M2
+        for (int i = 0; i < M2.getFinalStates().size(); i++) {
+            _finalStates.push_back(M2.getFinalStates()[i] + M1.getSize() + 1);
+        }
+
+        // add transition from new start state to old M2 start state
+        _delta[M2.getStart() + M1.getSize() + 1].push_back({ 0,'&' });
+
+
+    }
+
+    // CONCATENATION
+    else if (op == '.') {
+        // copy M1 start, delta, and size to new NFA
+        _size = M1.getSize();
+        _delta = M1.getDelta();
+        _startState = M1.getStart();    
+
+        // copy M2 into new NFA with adjusted states
+        for (int i = 0; i < M2.getSize(); i++) {
+            vector<pair<int, char>> temp = M2.getDelta()[i];    // get first vector of pairs from M2
+            
+            // increment each state num in vector of pairs by size of M1
+            for (int j = 0; j < temp.size(); j++) {
+                get<0>(temp[j]) += M1.getSize();
+            }
+
+            _delta.push_back(temp);     // push modified vector of M2 pairs 
+            _size++;    // increment size
+        }
+
+        // add epsilon transitions from final states of M1 to start state of M2
+        for (int i = 0; i < M1.getFinalStates().size(); i++) {
+            // add pair at M2 start state as it is represented in new NFA
+            _delta[M2.getStart() + M1.getSize()].push_back({M1.getFinalStates()[i],'&'});
+        }
+
+        // make current final states the same as M2 final states + M1.size
+        for (int i = 0; i < M2.getFinalStates().size(); i++) {
+            _finalStates.push_back(M2.getFinalStates()[i] + M1.getSize());
+        }
+
+    }
+
+    // KLEEN STAR
+    else if (op == '*') {
+        // NOTE: This only modifies a copy of NFA M1 and doesn't use M2
+
+        // copy M1 start, delta, finals, and size to new NFA
+        _size = M1.getSize();
+        _delta = M1.getDelta();
+        _startState = M1.getStart();   
+        _finalStates = M1.getFinalStates();    
+
+        // allow for empty string by mapping start state to each final state with an epsilon transition
+        for (int i = 0; i < M1.getFinalStates().size(); i++) {
+            // allow for empty string by mapping start state to each final state with an epsilon transition
+            _delta[M1.getFinalStates()[i]].push_back({ _startState,'&'});
+            // allow a loop of the NFA by mapping final states to start state 
+            _delta[_startState].push_back({ M1.getFinalStates()[i],'&' });
+        }
     }
 }
 
-int main() {
-    /*
-    string RE = "(A+B.C)*";
-    string prefix = infixToPrefix(RE);
-    cout << prefix << endl;
-    */
-    NFA M1('a');
-    M1.print();
-
-
-    return 0;
+void NFA::print() {
+    for (int i = 0; i < _delta.size(); i++) {
+        cout << "Result in State " << i << ": ";
+        for (int j = 0; j < _delta[i].size(); j++) {
+            cout << "(" << get<0>(_delta[i][j]) << ", " << get<1>(_delta[i][j]) << ") ";
+        }
+        cout << endl;
+    }
 }
