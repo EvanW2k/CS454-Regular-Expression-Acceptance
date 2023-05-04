cc39ba132a34e46038dfdfc506023b09b2705fc0
EvanW2k <125333331+EvanW2k@users.noreply.github.com>
2023-05-03 19:46:43 -0700

    Update NFA.hpp

--- NFA.hpp
+++ NFA.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <utility> // pair
 #include <stack>
 #include <string>
 #include <assert.h>
@@ -13,27 +14,32 @@
 
 class NFA {
 public:
+    NFA();
     NFA(char operand);
-    NFA(std::vector<int> finalStates, std::vector<std::vector<char>> delta) : _finalStates{finalStates}, _delta{delta} {};
-    
-    //_delta = Union of M1 + M2
-    NFA(NFA M1, NFA M2, char op);
+    //NFA(std::vector<int> finalStates, std::vector<std::vector<char>> delta) : _finalStates{ finalStates }, _delta{ delta } {};
 
+    //_delta = Union of M1 + M2
+    NFA(NFA M1, char op, NFA M2 = NFA());
+
+    ~NFA() {};
     int getSize() { return _size; }
+    int getStart() { return _startState; }
     std::vector<int> getFinalStates() { return _finalStates; }
-    std::vector<std::vector<char>> getDelta() { return _delta; }
+    std::vector<std::vector<std::pair<int, char>>> getDelta() { return _delta; }
     void setFinalStates(std::vector<int> final) { _finalStates = final; }
-    void setDelta(std::vector<std::vector<char>> delta) { _delta = delta; }
+    void setDelta(std::vector<std::vector<std::pair<int, char>>> delta) { _delta = delta; }
 
     //i is starting position of current index, RE is prefix string containing input symbols
     //recursive
     std::pair<NFA, int> convert(int i, std::string RE);
 
     void print();
+
 private:
     std::vector<int> _finalStates;
+    int _startState = 0;
     int _size = 0;
-    //change to hashtable
-    std::vector<std::vector<char>> _delta;
+    // 2d vector of pairs (state, transition)
+    std::vector<std::vector<std::pair<int, char>>> _delta;
 
 };
